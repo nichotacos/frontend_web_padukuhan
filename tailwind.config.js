58035e9f1614f463a06f8e103d2852634d789412
Nicholas Yang <nicho.yeo7@gmail.com>
2024-06-27 02:03:34 +0700

    adding more fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+        'bebas-neue': ['"Bebas Neue"', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'duwet-main-bg': '#FCFAF9',
+        'duwet-primary': '#386641',
+        'duwet-secondary': '#A7C957',
+        'duwet-accent': '#F2D246',
+        'duwet-dark-text': '#313131'
+      }
+    },
   },
   plugins: [],
 }
