f7cda4e62f6cc9bbc4f78fa63f145c92355e8a07
Nicholas Yang <nicho.yeo7@gmail.com>
2024-06-27 02:03:59 +0700

    hero page done

---
+++ src/pages/HeroPage.jsx
@@ -0,0 +1,11 @@
+
+
+export default function HeroPage() {
+    return (
+        <>
+            <p className="text-7xl text-white font-poppins font-bold mt-5">Selamat Datang</p>
+            <p className="text-5xl text-white font-poppins font-bold mt-5">Di Padukuhan Duwet</p>
+            <p className="text-4xl text-white font-poppins font-bold mt-5">Jerukwudel, Girisubo</p>
+        </>
+    )
+}
