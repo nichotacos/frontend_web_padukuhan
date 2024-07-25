55227dcedc6dee60f48849a25e1a68e138f90b4e
Nicholas Yang <nicho.yeo7@gmail.com>
2024-07-25 18:26:40 +0700

    fix typo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
         <Navbar shouldHideOnScroll isBlurred={false} className='bg-duwet-main-bg' >
             <NavbarBrand className='flex flex-row align-middle'>
                 <img src={logo} alt="Logo" className="w-20 h-w-20 mr-4" />
-                <p className="font-bold font-bebas-neue text-3xl text-inherit">PADUKUHUAN DUWET</p>
+                <p className="font-bold font-bebas-neue text-3xl text-inherit">PADUKUHAN DUWET</p>
             </NavbarBrand>
             <NavbarContent className="hidden sm:flex gap-4" justify="end">
                 <NavbarItem isActive>
