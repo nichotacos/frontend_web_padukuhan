5ace00a367bd12e507e0b537b469d1d240f61a78
Nicholas Yang <nicho.yeo7@gmail.com>
2024-07-27 22:36:24 +0700

    current progress

--- src/pages/AllContents.jsx
+++ src/pages/AllContents.jsx
@@ -4,6 +4,7 @@ import { FetchKonten } from "../api/ApiContent";
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import Loader from "../components/Loading";
+import moment from "moment";
 
 export default function AllContents() {
 
@@ -43,19 +44,20 @@ export default function AllContents() {
         <>
             <div className="bg-duwet-main-bg flex flex-row justify-center align-middle">
                 <div className="mt-8 w-3/4 flex flex-col justify-center items-center align-middle mb-8">
-                    <p className="font-poppins text-4xl font-semibold mb-8 text-white w-1/3 text-center bg-duwet-primary py-4 rounded-2xl">Semua Berita</p>
+                    <p className="font-poppins text-2xl md:text-2xl lg:text-4xl font-semibold mb-8 text-white w-5/6 lg:w-1/3 text-center bg-duwet-primary py-2 lg:py-4 rounded-2xl">Semua Berita</p>
                     <div className="flex flex-row gap-8 flex-wrap justify-evenly ">
                         {contents.map((content) => (
-                            <Card key={content.id} shadow className="w-72">
+                            <Card key={content.id} shadow="lg" className="w-64 lg:w-72">
                                 <CardHeader>
                                     <Image src={content.image} alt={content.title} className="aspect-square" />
                                 </CardHeader>
                                 <CardBody>
                                     <p className="font-poppins font-bold text-lg">{content.title}</p>
-                                    <p className="truncate">{content.content}</p>
+                                    <p className="line-clamp-3 text-sm mt-2">{content.content}</p>
                                 </CardBody>
-                                <CardFooter>
-                                    <Button auto color="warning" className="font-semibold" onClick={() => handleReadMore(content.id)}>Read More</Button>
+                                <CardFooter className="flex flex-row justify-between">
+                                    <Button auto className="font-semibold bg-amber-500 text-white" onClick={() => handleReadMore(content.id)}>Baca Berita</Button>
+                                    <p className="font-inter text-xs text-pretty ">{moment(content.created_at).format('LL')}</p>
                                 </CardFooter>
                             </Card>
                         ))}
